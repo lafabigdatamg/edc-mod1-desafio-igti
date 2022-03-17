519d6b6c2c1ad5671d1c1068eddef2db68ac5b2e
lafabigdatamg <bigdata.lafamg@outlook.com>
2022-03-17 14:46:54 -0300

    add infra para airflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,15 @@ variable "aws_region" {
 variable "lambda_function_name" {
   default = "RAISexecutaEMR"
 }
+
+variable "key_pair_name" {
+  default = "eric-igti-teste"
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-023bffa8b7a996029"
+}
+
+variable "vpc_id" {
+  default = "vpc-049efbdc9209d321b"
+}
